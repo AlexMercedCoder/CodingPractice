afb53f9f3d60d97540bd0a5753d46607a084093b
Alex Merced - AlexMercedCoder.com <alexmerced@alexmerced.com>
2019-12-13 14:32:03 -0500

    string methods

---
+++ stringmethods.js
@@ -0,0 +1,22 @@
+//toUppercase, toLowercase, charAt, concat, endswith, includes,match,repeat, replace,trim
+
+world = 'world'
+myString = 'Hello '
+
+
+world = world.toUpperCase()
+myString = myString.toLowerCase()
+
+//concatenation
+
+console.log(myString+world)
+
+//interpolation
+
+console.log(`${myString + world}, I am doing well today`)
+
+console.log(myString.charAt(4))
+
+console.log(myString.endsWith('hello'))
+
+myString.split()
